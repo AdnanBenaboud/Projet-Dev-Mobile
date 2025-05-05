36870be5a18325460610421c80bad06474a8cac9
adnan <adnan@gmail.com>
2025-05-05 16:39:26 +0100

    add mapbox and test it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.mapbox.maps:android:11.11.0")
 }
--- app/src/main/java/com/example/projetdevmobile/MainActivity.java
+++ app/src/main/java/com/example/projetdevmobile/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.projetdevmobile;
 
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsCompat.Type;
+import androidx.core.graphics.Insets;
+
+
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.CameraOptions;
+import com.mapbox.maps.MapView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        mapView = findViewById(R.id.mapView);
+
+        // Set initial camera position
+        CameraOptions cameraOptions = new CameraOptions.Builder()
+                .center(Point.fromLngLat(-98.0, 39.5))
+                .pitch(0.0)
+                .zoom(2.0)
+                .bearing(0.0)
+                .build();
+
+        mapView.getMapboxMap().setCamera(cameraOptions);
     }
 }
